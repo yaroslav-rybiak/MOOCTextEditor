1b19dd052a9f08ef287809aa331624a812801b76
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-04-08 23:43:26 +0200

    start working with linked list

--- src/textgen/MyLinkedList.java
+++ src/textgen/MyLinkedList.java
@@ -3,98 +3,109 @@ package textgen;
 import java.util.AbstractList;
 
 
-/** A class that implements a doubly linked list
- * 
- * @author UC San Diego Intermediate Programming MOOC team
+/**
+ * A class that implements a doubly linked list
  *
  * @param <E> The type of the elements stored in the list
+ * @author UC San Diego Intermediate Programming MOOC team
  */
 public class MyLinkedList<E> extends AbstractList<E> {
-	LLNode<E> head;
-	LLNode<E> tail;
-	int size;
+    LLNode<E> head;
+    LLNode<E> tail;
+    int size;
 
-	/** Create a new empty LinkedList */
-	public MyLinkedList() {
-		// TODO: Implement this method
-	}
+    /**
+     * Create a new empty LinkedList
+     */
+    MyLinkedList() {
+        size = 0;
+        head = new LLNode<>(null);
+        tail = new LLNode<>(null);
+        head.next = tail;
+        tail.prev = head;
+    }
 
-	/**
-	 * Appends an element to the end of the list
-	 * @param element The element to add
-	 */
-	public boolean add(E element ) 
-	{
-		// TODO: Implement this method
-		return false;
-	}
+    /**
+     * Appends an element to the end of the list
+     *
+     * @param element The element to add
+     */
+    public boolean add(E element) {
+        if (element == null) throw new NullPointerException();
+        // TODO: Implement this method
+        return false;
+    }
 
-	/** Get the element at position index 
-	 * @throws IndexOutOfBoundsException if the index is out of bounds. */
-	public E get(int index) 
-	{
-		// TODO: Implement this method.
-		return null;
-	}
+    /**
+     * Get the element at position index
+     *
+     * @throws IndexOutOfBoundsException if the index is out of bounds.
+     */
+    public E get(int index) {
+        if (index >= size || index < 0) throw new IndexOutOfBoundsException();
+        // TODO: Implement this method
+        return null;
+    }
 
-	/**
-	 * Add an element to the list at the specified index
-	 * @param The index where the element should be added
-	 * @param element The element to add
-	 */
-	public void add(int index, E element ) 
-	{
-		// TODO: Implement this method
-	}
+    /**
+     * Add an element to the list at the specified index
+     *
+     * @param index   where the element should be added
+     * @param element The element to add
+     */
+    public void add(int index, E element) {
+        if (index > size || index < 0) throw new IndexOutOfBoundsException();
+        // TODO: Implement this method
+    }
 
 
-	/** Return the size of the list */
-	public int size() 
-	{
-		// TODO: Implement this method
-		return -1;
-	}
+    /**
+     * Return the size of the list
+     */
+    public int size() {
+        return size;
+    }
 
-	/** Remove a node at the specified index and return its data element.
-	 * @param index The index of the element to remove
-	 * @return The data element removed
-	 * @throws IndexOutOfBoundsException If index is outside the bounds of the list
-	 * 
-	 */
-	public E remove(int index) 
-	{
-		// TODO: Implement this method
-		return null;
-	}
+    /**
+     * Remove a node at the specified index and return its data element.
+     *
+     * @param index The index of the element to remove
+     * @return The data element removed
+     * @throws IndexOutOfBoundsException If index is outside the bounds of the list
+     */
+    public E remove(int index) {
+        if (index >= size || index < 0) throw new IndexOutOfBoundsException();
+        // TODO: Implement this method
+        return null;
+    }
 
-	/**
-	 * Set an index position in the list to a new element
-	 * @param index The index of the element to change
-	 * @param element The new element
-	 * @return The element that was replaced
-	 * @throws IndexOutOfBoundsException if the index is out of bounds.
-	 */
-	public E set(int index, E element) 
-	{
-		// TODO: Implement this method
-		return null;
-	}   
+    /**
+     * Set an index position in the list to a new element
+     *
+     * @param index   The index of the element to change
+     * @param element The new element
+     * @return The element that was replaced
+     * @throws IndexOutOfBoundsException if the index is out of bounds.
+     */
+    public E set(int index, E element) {
+        if (index > size || index < 0) throw new IndexOutOfBoundsException();
+        // TODO: Implement this method
+        return null;
+    }
 }
 
-class LLNode<E> 
-{
-	LLNode<E> prev;
-	LLNode<E> next;
-	E data;
+class LLNode<E> {
+    LLNode<E> prev;
+    LLNode<E> next;
+    E data;
 
-	// TODO: Add any other methods you think are useful here
-	// E.g. you might want to add another constructor
+    // TODO: Add any other methods you think are useful here
+    // E.g. you might want to add another constructor
 
-	public LLNode(E e) 
-	{
-		this.data = e;
-		this.prev = null;
-		this.next = null;
-	}
+    public LLNode(E e) {
+        this.data = e;
+        this.prev = null;
+        this.next = null;
+    }
 
 }
