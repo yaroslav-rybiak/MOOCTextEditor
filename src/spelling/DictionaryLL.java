1168c281d695e9ea63359745f46fba933ccbfafd
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-04-10 21:56:04 +0200

    implementing linked list dictionary

--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -4,39 +4,41 @@ import java.util.LinkedList;
 
 /**
  * A class that implements the Dictionary interface using a LinkedList
- *
  */
-public class DictionaryLL implements Dictionary 
-{
+public class DictionaryLL implements Dictionary {
 
-	private LinkedList<String> dict;
-	
-    // TODO: Add a constructor
+    private LinkedList<String> dict;
 
+    public DictionaryLL() {
+        dict = new LinkedList<>();
+    }
 
-    /** Add this word to the dictionary.  Convert it to lowercase first
+    /**
+     * Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
+     *
      * @param word The word to add
-     * @return true if the word was added to the dictionary 
-     * (it wasn't already there). */
+     * @return true if the word was added to the dictionary
+     * (it wasn't already there).
+     */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+        word = word.toLowerCase();
+        return !isWord(word) && dict.add(word);
     }
 
 
-    /** Return the number of words in the dictionary */
-    public int size()
-    {
-        // TODO: Implement this method
-        return 0;
+    /**
+     * Return the number of words in the dictionary
+     */
+    public int size() {
+        return dict.size();
     }
 
-    /** Is this a word according to this dictionary? */
+    /**
+     * Is this a word according to this dictionary?
+     */
     public boolean isWord(String s) {
-        //TODO: Implement this method
-        return false;
+        s = s.toLowerCase();
+        return dict.contains(s);
     }
-
-    
 }
