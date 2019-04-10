abfe4a27a903fc02e14745edc53979559d1c6f06
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-04-10 21:56:22 +0200

    implementing binary search tree dictionary

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -4,38 +4,39 @@ import java.util.TreeSet;
 
 /**
  * @author UC San Diego Intermediate MOOC team
- *
  */
-public class DictionaryBST implements Dictionary 
-{
-   private TreeSet<String> dict;
-	
-    // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
-	
-    
-    /** Add this word to the dictionary.  Convert it to lowercase first
+public class DictionaryBST implements Dictionary {
+    private TreeSet<String> dict;
+
+    public DictionaryBST() {
+        dict = new TreeSet<>();
+    }
+
+    /**
+     * Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
+     *
      * @param word The word to add
-     * @return true if the word was added to the dictionary 
-     * (it wasn't already there). */
+     * @return true if the word was added to the dictionary
+     * (it wasn't already there).
+     */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+        word = word.toLowerCase();
+        return !isWord(word) && dict.add(word);
     }
 
-
-    /** Return the number of words in the dictionary */
-    public int size()
-    {
-    	// TODO: Implement this method
-        return 0;
+    /**
+     * Return the number of words in the dictionary
+     */
+    public int size() {
+        return dict.size();
     }
 
-    /** Is this a word according to this dictionary? */
+    /**
+     * Is this a word according to this dictionary?
+     */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+        s = s.toLowerCase();
+        return dict.contains(s);
     }
-
 }
